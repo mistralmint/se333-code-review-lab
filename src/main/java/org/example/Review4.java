c18adfa451fe477afe941fcf307b9565ce7204e0
mistralmint <mwmuriel@gmail.com>
2025-10-14 14:16:24 -0500

    fix: remove redundant null check

--- src/main/java/org/example/Review4.java
+++ src/main/java/org/example/Review4.java
@@ -3,7 +3,7 @@ package org.example;
 public class Review4 {
     public void method(){
         Integer i = Integer.valueOf(400);
-        if (i == null)
-            throw new NullPointerException();
+        /*if (i == null)
+            throw new NullPointerException();*/
     }
 }
