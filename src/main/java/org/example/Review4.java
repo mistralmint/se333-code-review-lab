9718c29dc53d889f6629cb43b9f0aebfccbe5551
Matthew Muriel <mwmuriel@gmail.com>
2025-10-21 22:44:12 -0500

    Delete src/main/java/org/example/Review4.java

--- src/main/java/org/example/Review4.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Review4 {
-    public void method(){
-        Integer i = Integer.valueOf(400);
-        /*if (i == null)
-            throw new NullPointerException();*/
-    }
-}
